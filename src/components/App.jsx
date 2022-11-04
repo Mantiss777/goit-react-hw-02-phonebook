d469490c9ee5ebffd6ffe4cdf90d65708d894181
Oleksandr Tsabak <sasha.tsabak@gmail.com>
2022-11-04 15:54:21 +0200

    step 2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,56 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React from 'react';
+import { Box } from './Box';
+import Section from './Section/Section';
+// import FeedbackOptions from './Feedback/Feedback';
+// import Statistics from './Statistics/Statistics';
+
+// import Notification from './Notification/Notification';
+
+export class App extends React.Component {
+  state = {
+    contacts: [],
+    name: '',
+  };
+
+  handleNameChange = event => {
+    console.log(event.currentTarget.value);
+    this.setState({ name: event.currentTarget.value });
+  };
+
+  render() {
+    return (
+      <Box position="relative" as="main">
+        <Section title="Please leave feedback">Phonebook </Section>
+        <Box>
+          <form>
+            <label>
+              Name
+              <input
+                type="text"
+                value={this.state.name}
+                onChange={this.handleNameChange}
+              />
+            </label>
+          </form>
+        </Box>
+
+        <Section title="Statistics">Contacts </Section>
+        {/* {!this.state.visible && (
+          <Notification message="There is no feedback">
+            There is no feedback
+          </Notification>
+        )}
+
+        {this.state.visible && (
+          <Statistics
+            good={this.state.good}
+            neutral={this.state.neutral}
+            bad={this.state.bad}
+            total={this.state.total}
+            positivePercentage={this.state.positiveFeedbeck}
+          />
+        )} */}
+      </Box>
+    );
+  }
+}
